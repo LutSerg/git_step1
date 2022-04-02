62b44b7eb6515377662ffb91ff4bb3aa097bbda7
LutSerg <asteroster@gmail.com>
2022-04-02 14:43:40 +0300

    added new symbols

--- src/qa/asteroster/Main.java
+++ src/qa/asteroster/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("slow branch");
     }
     public static void another() {
-        System.out.println("anoter method");
+
+        System.out.println("another method!");
     }
 }
