8bc295937237b84c6e6092859cceee00f97c9c13
Sergey Lut <102815800+LutSerg@users.noreply.github.com>
2022-04-02 14:19:10 +0300

    Update Main.java

--- src/qa/asteroster/Main.java
+++ src/qa/asteroster/Main.java
@@ -6,4 +6,7 @@ public class Main {
         System.out.println("Hey, QA GURU!");
         System.out.println("this is another line in my code");
     }
+    public static void another() {
+        System.out.println("anoter method");
+    }
 }
